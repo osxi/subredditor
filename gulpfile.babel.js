6e832c89723e6f11a6fe865edc3ffe8a187bf2c4
osxi <osxi@users.noreply.github.com>
2015-08-24 22:54:27 -0500

    fix: --subreddit runs reliably the first time

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,15 @@
 'use strict';
 
-import argv from 'yargs';
 import del from 'del';
 import fs from 'fs';
 import gulp from 'gulp';
+import _ from 'lodash';
 import minifyCss from 'gulp-minify-css';
 import request from 'request';
+import RSVP from 'rsvp';
 import sass from 'gulp-sass';
 import server from 'gulp-server-livereload';
+import yargs from 'yargs';
 
 var defaultFile = 'subreddit.html';
 
@@ -20,35 +22,44 @@ var paths = {
 gulp.task('default', ['server']);
 
 gulp.task('sass', () => {
-  gulp.src(paths.sassGlob)
+  return gulp.src(paths.sassGlob)
     .pipe(sass().on('error', sass.logError))
     .pipe(minifyCss({compatibility: 'ie8'}))
     .pipe(gulp.dest(paths.distDir));
 });
 
 gulp.task('sass:watch', () => {
-  gulp.watch(paths.sassGlob, ['sass']);
+  return gulp.watch(paths.sassGlob, ['sass']);
 });
 
 gulp.task('server', ['sass'], () => {
   gulp.watch(paths.sassGlob, ['sass']);
 
-  gulp.src([paths.srcGlob, paths.distDir])
+  return gulp.src([paths.srcGlob, paths.distDir])
     .pipe(server({
       defaultFile: defaultFile,
       livereload: true,
-      open: true
+      open: false
     }));
 });
 
-gulp.task('clean', () => {
-  del(['src/custom.scss', './dist/**'] , (err, paths) => {
-    console.log('Deleted files/folders:\n', paths.join('\n'));
-  });
+gulp.task('download', () => {
+  return clean().then(downloadCss.bind(this, paths));
 });
 
-gulp.task('download', ['clean'], () => {
-  var url = argv.subreddit;
+function clean() {
+  return new RSVP.Promise((resolve, reject) => {
+    del(['src/custom.scss', './dist/**'] , (err, paths) => {
+      if (err) {reject(err)}
+      resolve(paths);
+    });
+  });
+};
+
+function downloadCss(pathsCleaned) {
+  var url = yargs.argv.subreddit;
+
+  console.log('Deleted files/folders:\n', _.values(pathsCleaned).join(', '));
 
   if (url === undefined) {
     console.log('please specify --subreddit, e.g. --subreddit=node');
@@ -57,6 +68,5 @@ gulp.task('download', ['clean'], () => {
 
   url = `http://reddit.com/r/${url}/stylesheet.css`;
 
-  return request(url)
-    .pipe(fs.createWriteStream('./src/custom.scss'));
-});
+  return request(url).pipe(fs.createWriteStream('./src/custom.scss'));
+}
